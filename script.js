be39d6bf2d547957c822c582e7c9ad54ad62de7e
Jarvis Lee <jarvisgaming40@gmail.com>
2026-02-28 14:01:40 +0800

    Implement basic game board logic

--- script.js
+++ script.js
@@ -5,20 +5,81 @@ const TicTacToe = function(){
         Empty: " ",
     };
 
+    const Player = {
+        X: "Player1",
+        O: "Player2",
+    };
+
+    const Winner = {
+        X: "Player1",
+        O: "Player2",
+        Tie: "Tie",
+        Undetermined: "Undetermined",
+    };
+
     const GameBoard = function(){
-        const state = new Array.length(9).fill(Symbol.Empty);
+        const boardSize = 9;
+        const state = new Array(boardSize).fill(Symbol.Empty);
 
         function isEmpty(position){
             return state[position] == Symbol.Empty;
         }
 
+        // Returns true if the mark is successfully placed, false otherwise.
         function set(position, mark){
+            if (!isEmpty(position)) return false;
             state[position] = mark;
+            return true;
+        }
+
+        function display(){
+            const rowSeparator = "+-+-+-+\n";
+            function buildRow(mark1, mark2, mark3){
+                return `|${mark1}|${mark2}|${mark3}|\n`;
+            }
+
+            let output = "";
+            for (let i = 0; i <= 6; i += 3){
+                output += rowSeparator;
+                output += buildRow(state[i], state[i+1], state[i+2]);
+            }
+            output += rowSeparator;
+            console.log(output);
+        }
+
+        function getWinner(){
+            const winningPaths = [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8],
+                [0, 4, 8],
+                [2, 4, 6],
+            ];
+
+            function isMarksEqual(mark1, mark2, mark3, target){
+                return (mark1 == mark2) &&
+                    (mark2 == mark3) &&
+                    (mark3 == target);
+            }
+
+            for (const [pos1, pos2, pos3] of winningPaths){
+                const [mark1, mark2, mark3] = [state[pos1], state[pos2], state[pos3]];
+
+                if (isMarksEqual(mark1, mark2, mark3, Symbol.X)) return Winner.X;
+                else if (isMarksEqual(mark1, mark2, mark3, Symbol.O)) return Winner.O;
+            }
+
+            if (state.filter(mark => mark != Symbol.Empty).length == boardSize) return Winner.Tie;
+            else return Winner.Undetermined;
         }
 
         return {
-            isEmpty,
             set,
+            display,
+            getWinner,
         };
     }();
 }();
